7b004bfbb6f33ad9e43bac3db107e1ec80acaa8c
Vicente Gómez Medina <97600445+vicentegm2@users.noreply.github.com>
2025-09-30 11:01:52 +0200

    feat(home): add profile picture to presentation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+type Theme = 'light' | 'dark';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule], 
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('angular-micro-portfolio');
+  theme = signal<Theme>('light'); 
+
+  ngOnInit() {
+    const saved = localStorage.getItem('theme') as Theme;
+    this.theme.set(saved === 'dark' ? 'dark' : 'light'); 
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    const newTheme: Theme = this.theme() === 'light' ? 'dark' : 'light';
+    this.theme.set(newTheme);
+    
+    localStorage.setItem('theme', newTheme);
+    
+    this.applyTheme();
+  }
+
+  private applyTheme() {
+    const root = document.documentElement; 
+    
+    if (this.theme() === 'dark') {
+      root.setAttribute('data-theme', 'dark');
+    } else {
+      root.removeAttribute('data-theme');
+    }
+  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -7,14 +7,12 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   template: `
     <section style="padding:3rem 1.25rem; text-align:center">
+      <img src="../../assets/profile-2.jpg" alt="profile picture" class="profile-pic" />
+
       <h1>Hola, soy Vicente 👋</h1>
       <p>Frontend learner — Angular & React. Subo mini-proyectos a GitHub.</p>
-      <a
-        routerLink="/projects"
-        style="display:inline-block;margin-top:1rem;padding:.6rem 1rem;border:1px solid #111;border-radius:10px;text-decoration:none;color:inherit"
-      >
-        Ver proyectos
-      </a>
+
+      <a routerLink="/projects" class="btn">Ver proyectos</a>
     </section>
   `,
   styles: [
@@ -29,6 +27,15 @@ import { RouterModule } from '@angular/router';
         padding: 0.6rem 1rem;
         border: 1px solid #111;
         border-radius: 10px;
+      }
+      .profile-pic {
+        width: 140px;
+        height: 140px;
+        border-radius: 50%;
+        object-fit: cover;
+        border: 3px solid var(--border);
+        display: block;
+        margin: 0 auto 1rem auto;
       }
     `,
   ],
